38b99afc798f0890b1e7f3564a52eadfb0d6b9cc
Banner <banner.schafer@gmail.com>
2014-02-14 14:56:46 -0800

    remove folder dir

--- folder/db.conf.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql'); 
-
-exports.db = mysql.createConnection ({ 
-		host     : 'localhost', 
-		user     : 'user', 
-		password : 'supersecret', 
-		database : 'database' 
-}); 
